d43fae3d9d0eea9cb0a3284f0f993a60b6db0852
Morteza Nasiri <n.mortezapc@gmail.com>
2021-02-21 21:32:11 +0330

    Add Log Model

---
+++ DocumentRoot/app/Models/Log.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Log extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'ip',
+        'verb',
+        'endpoint',
+        'request',
+        'response',
+        'code',
+    ];
+
+    /**
+     * The storage format of the model's date column
+     *
+     * @var string
+     */
+    protected $dateFormat = 'Y-m-d H:i:s';
+}
