d26298098c730723c84e14c126a18e21b397a2ea
Morteza Nasiri <n.mortezapc@gmail.com>
2021-02-21 21:43:08 +0330

    Add CommentCreateRequest.php

---
+++ DocumentRoot/app/Http/Requests/CommentCreateRequest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+
+class CommentCreateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'user_id' => 'required|exists:users,id',
+            'product_id' => 'required|exists:products,id',
+            'description' => 'required|string|min:10',
+        ];
+    }
+
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            'user_id' => 'user id',
+            'product_id' => 'product id',
+            'description' => 'comment content',
+        ];
+    }
+
+    /**
+     * @param Validator $validator
+     */
+    protected function failedValidation(Validator $validator): void
+    {
+        $errors = $validator->errors()->toArray();
+        $messages = [];
+
+        foreach ($errors as $error) {
+            $messages[] = $error[0];
+        }
+
+        throw new HttpResponseException(response()->json([
+            'message' => $messages
+        ], 422, [], JSON_UNESCAPED_UNICODE)
+        );
+    }
+}
