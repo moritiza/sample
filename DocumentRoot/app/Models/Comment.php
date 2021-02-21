fdbafa3f900b1660736d695ff4c19e08781f0217
Morteza Nasiri <n.mortezapc@gmail.com>
2021-02-21 21:46:19 +0330

    Add methods

--- DocumentRoot/app/Models/Comment.php
+++ DocumentRoot/app/Models/Comment.php
@@ -8,4 +8,47 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'product_id',
+        'approve',
+        'description',
+        'created_at'
+    ];
+
+    /**
+     * The storage format of the model's date column
+     *
+     * @var string
+     */
+    protected $dateFormat = 'Y-m-d H:i:s';
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * Get the comment owner user
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the comment owner product
+     */
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
