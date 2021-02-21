be72f2e4a760e0a9f49a4e22402153be9cf0b807
Morteza Nasiri <n.mortezapc@gmail.com>
2021-02-21 21:44:41 +0330

    Add methods

--- DocumentRoot/app/Models/Vote.php
+++ DocumentRoot/app/Models/Vote.php
@@ -8,4 +8,47 @@ use Illuminate\Database\Eloquent\Model;
 class Vote extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'product_id',
+        'approve',
+        'vote',
+        'created_at'
+    ];
+
+    /**
+     * The storage format of the model's date column
+     *
+     * @var string
+     */
+    protected $dateFormat = 'Y-m-d H:i:s';
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * Get the vote owner user
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the vote owner product
+     */
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
