46838db1d9c123a5301b917d73d51ff72a16e288
Morteza Nasiri <n.mortezapc@gmail.com>
2021-02-21 21:46:48 +0330

    Add routes

--- DocumentRoot/routes/api.php
+++ DocumentRoot/routes/api.php
@@ -17,3 +17,62 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/*
+|--------------------------------------------------------------------------
+| Management Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::namespace('App\Http\Controllers\Api\V1\Management')
+    ->prefix('v1/management')
+    ->group(
+        function () {
+            /* ------------------- Product Routes ------------------- */
+            Route::get('/products', 'ProductController@index');
+            Route::put('/products/{id}', 'ProductController@update');
+
+            /* ------------------- Vote Routes ------------------- */
+            Route::get('/votes', 'VoteController@index');
+            Route::put('/votes/{id}', 'VoteController@update');
+
+            /* ------------------- Comment Routes ------------------- */
+            Route::get('/comments', 'CommentController@index');
+            Route::put('/comments/{id}', 'CommentController@update');
+        }
+    );
+
+/*
+|--------------------------------------------------------------------------
+| Management Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::namespace('App\Http\Controllers\Api\V1\User')
+    ->prefix('v1/user')
+    ->group(
+        function () {
+            /* ------------------- Product Routes ------------------- */
+            Route::get('/products', 'ProductController@index');
+            Route::get('/products/{id}', 'ProductController@show');
+
+            /* ------------------- Review Routes ------------------- */
+            Route::get('/products/{id}/control', 'ReviewController@control');
+            Route::get('/products/{id}/rates', 'ReviewController@rates');
+            Route::get('/products/{id}/comments', 'ReviewController@lastComments');
+
+            /* -------------------  Routes ------------------- */
+            Route::post('/comments', 'FeedbackController@storeComment');
+            Route::post('/votes', 'FeedbackController@storeVote');
+        }
+    );
+
+/*
+|--------------------------------------------------------------------------
+| PingPong Route
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/ping', function () {
+    return response()->json('pong', 200);
+});
