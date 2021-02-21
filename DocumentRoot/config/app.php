0dc647bac7c524d72460320fb195e51f9d65cd05
Morteza Nasiri <n.mortezapc@gmail.com>
2021-02-21 21:46:37 +0330

    Add response service provider

--- DocumentRoot/config/app.php
+++ DocumentRoot/config/app.php
@@ -175,6 +175,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Custom Providers...
+         */
+        App\Providers\ResponseServiceProvider::class,
+
     ],
 
     /*
