43bf43f94c06a97f1c9b019730bce9da99b690d7
Morteza Nasiri <n.mortezapc@gmail.com>
2021-02-21 21:45:00 +0330

    Add methods

--- DocumentRoot/app/Models/Provider.php
+++ DocumentRoot/app/Models/Provider.php
@@ -8,4 +8,35 @@ use Illuminate\Database\Eloquent\Model;
 class Provider extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+    ];
+
+    /**
+     * The storage format of the model's date column
+     *
+     * @var string
+     */
+    protected $dateFormat = 'Y-m-d H:i:s';
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * Get the products for the provider
+     */
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
