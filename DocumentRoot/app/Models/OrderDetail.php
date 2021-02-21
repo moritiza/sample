59c27624f88085d21f1f8fd57d73f5b611c5269a
Morteza Nasiri <n.mortezapc@gmail.com>
2021-02-21 21:45:14 +0330

    Add methods

--- DocumentRoot/app/Models/OrderDetail.php
+++ DocumentRoot/app/Models/OrderDetail.php
@@ -8,4 +8,36 @@ use Illuminate\Database\Eloquent\Model;
 class OrderDetail extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'order_id',
+        'product_id',
+    ];
+
+    /**
+     * The storage format of the model's date column
+     *
+     * @var string
+     */
+    protected $dateFormat = 'Y-m-d H:i:s';
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * Get the orderDetail owner order
+     */
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
 }
